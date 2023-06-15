1708ee77907cb1eb8d0e9dde5ce2558308cec126
Austin Stiefel <austin.stiefel@qualitytechsolutions.net>
2023-06-14 21:25:03 -0500

    removed clan members route with hardcoded clan tag

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { ClashRoyaleAPI } from '@varandas/clash-royale-api';
 
 dotenv.config();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 const api = new ClashRoyaleAPI(`${process.env.CLASH_ROYALE_API_TOKEN}`);
 const stats = express.Router();
 
+app.use(cors());
 app.use('/stats', stats);
 
 // Retrieve clan members from requested clan tag
@@ -17,17 +19,9 @@ stats.get('/clan/:tag/members', async (req, res) => {
     const members = await api.getClanMembers(`#${req.params.tag}`)
     res.json(members);
   } catch (error) {
-    res.status(500).json({error: 'Something went wrong'})
+    res.status(500).json({ error: 'Something went wrong' })
   }
-})
-stats.get('/stats/clanMembers', async (req, res) => {
-  try {
-    const members = await api.getClanMembers('#LG0RQC89');
-    res.json(members)
-  } catch (error) {
-    res.status(500).json({error: 'Something went wrong'})
-  }
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
